ee4bfc3a1dea174c61a529588d158dd0295155f6
re1apix <sonerknazev@gmail.com>
2019-10-28 18:50:10 +0400

    work

---
+++ SERVAAAAAAAAAAAAAAK/index2.js
@@ -0,0 +1,18 @@
+var express = require("express"),
+cors = require("cors"),
+fs= require("fs");
+var app = express();
+app.use(cors());
+app.get("/quote", function(req, res) {
+  fs.readFile("database.txt", "utf-8", function(err, content) {
+    let quotes = content.split("\n");
+    let index = Math.floor(Math.random() * (quotes.lenght - 1));
+    console.log(true);
+    let result = {
+      number: index + 1,
+      text: quotes[index]
+    };
+    res.send(JSON.stringify(result));
+  });
+});
+app.listen(590);
